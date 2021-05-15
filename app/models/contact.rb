4eee60cbdd43ee11c14228de9a47489ee2a6db34
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-05-14 18:17:03 -0600

    Add more tests for requests and add validations for job title
    Co-authored-by: Katy La Tour <23460878+klatour324@users.noreply.github.com>

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ApplicationRecord
   validates_presence_of :name, presence: true
   validates_presence_of :email, presence: true
-  validates_presence_of :phone_number, presence: true 
+  validates_presence_of :phone_number, presence: true
+  validates_presence_of :job_title, presence: true 
 end
