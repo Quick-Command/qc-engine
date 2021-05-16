46fe49befdef25ee99b806367d52b805a9f43d78
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-16 12:45:20 -0400

    Create incidents migration and csv file load for dev db

---
+++ db/migrate/20210516162248_create_incidents.rb
@@ -0,0 +1,15 @@
+class CreateIncidents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :incidents do |t|
+      t.string :name
+      t.boolean :active, default: true
+      t.string :incident_type
+      t.string :description
+      t.string :location
+      t.datetime :start_date
+      t.datetime :closed_date
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_15_000150) do
+ActiveRecord::Schema.define(version: 2021_05_16_162248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 2021_05_15_000150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "job_title"
+  end
+
+  create_table "incidents", force: :cascade do |t|
+    t.string "name"
+    t.boolean "active", default: true
+    t.string "incident_type"
+    t.string "description"
+    t.string "location"
+    t.datetime "start_date"
+    t.datetime "closed_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
