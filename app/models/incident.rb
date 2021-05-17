9cbed1da678a711dcc3282182deca70e4ca0bac0
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-17 09:14:20 -0400

    Add validations to incident model

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,6 @@
 class Incident < ApplicationRecord
+  validates_presence_of :name, presence: true
+  validates_presence_of :location, presence: true
+  validates_presence_of :start_date, presence: true
+  validates_presence_of :incident_type, presence: true 
 end
