8bc7276a8ff9db8f0927b10d4e8ed90f8320021d
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-18 08:41:36 -0400

    Add tests for Incident class method active

--- spec/models/incident_spec.rb
+++ spec/models/incident_spec.rb
@@ -7,4 +7,21 @@ RSpec.describe Incident, type: :model do
     it { should validate_presence_of :start_date }
     it { should validate_presence_of :incident_type }
   end
+
+  describe "::class_methods" do
+    describe "self.active(true/false)" do
+      it "returns incidents with active status set to true" do
+        create_list(:incident, 10, active: true)
+
+        expect(Incident.active(true).count).to eq(10)
+        expect(Incident.active(true).first.active).to eq(true)
+      end
+      it "returns incidents with active status set to false" do
+        create_list(:incident, 10, active: false)
+
+        expect(Incident.active(false).count).to eq(10)
+        expect(Incident.active(false).first.active).to eq(false)
+      end
+    end
+  end
 end
