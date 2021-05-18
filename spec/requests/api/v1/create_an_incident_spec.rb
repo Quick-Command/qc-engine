1799d7458f6c4e81c2946db769f9d91bde144acd
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-18 19:59:59 -0400

    Add tests for happy path and sad path create an incident

---
+++ spec/requests/api/v1/create_an_incident_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe "Create an incident" do
+  describe "happy path" do
+    it "creates an incident with active set to true as default" do
+      incident_params = ({
+        name: "Jim Creeks Fire",
+        incident_type: "Fire",
+        description: "5th alarm fire",
+        location: "Denver, CO",
+        start_date: "2020-05-01",
+        close_date: ""
+         })
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/incidents", headers: headers, params: JSON.generate(incident_params)
+      created_incident = Incident.last
+
+      expect(response).to be_successful
+      expect(created_incident.name).to eq(incident_params[:name])
+      expect(created_incident.active).to eq(true)
+      expect(created_incident.description).to eq(incident_params[:description])
+      expect(created_incident.incident_type).to eq(incident_params[:incident_type])
+      expect(created_incident.start_date).to eq(incident_params[:start_date])
+      expect(created_incident.close_date).to eq(incident_params[:close_date])
+
+      expect(response).to have_http_status(:created)
+      incident = JSON.parse(response.body, symbolize_names: true)
+    end
+    it "creates an incident with specified active status" do
+      incident_params = ({
+        name: "Jim Creeks Fire",
+        incident_type: "Fire",
+        active: false,
+        description: "5th alarm fire",
+        location: "Denver, CO",
+        start_date: "2020-05-01",
+        close_date: ""
+         })
+
+       headers = {"CONTENT_TYPE" => "application/json"}
+
+       post "/api/v1/incidents", headers: headers, params: JSON.generate(incident_params)
+       created_incident = Incident.last
+
+       expect(response).to be_successful
+       expect(created_incident.name).to eq(incident_params[:name])
+       expect(created_incident.active).to eq(false)
+       expect(created_incident.description).to eq(incident_params[:description])
+       expect(created_incident.incident_type).to eq(incident_params[:incident_type])
+       expect(created_incident.start_date).to eq(incident_params[:start_date])
+       expect(created_incident.close_date).to eq(incident_params[:close_date])
+
+       expect(response).to have_http_status(:created)
+       incident = JSON.parse(response.body, symbolize_names: true)
+    end
+   end
+
+  describe "sad path" do
+    it "Won't create a new incident with missing information" do
+      incident_params = ({
+        name: "Jim Creeks Fire",
+        active: false,
+        incident_type: "",
+        description: "5th alarm fire",
+        location: "Denver, CO",
+        start_date: "2020-05-01",
+        close_date: "2020-05-02"
+         })
+     headers = {"CONTENT_TYPE" => "application/json"}
+
+     post "/api/v1/incidents", headers: headers, params: JSON.generate(incident_params)
+
+     error = JSON.parse(response.body, symbolize_names:true)
+     error_message = "Validation failed: incident type, name, description, location, and start date cannot be blank"
+
+     expect(response).to have_http_status(:not_found)
+     expect(error).to have_key(:error)
+     expect(error[:error]).to eq("#{error_message}")
+    end
+  end
+end
