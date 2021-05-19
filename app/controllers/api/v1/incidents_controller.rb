398428ecd9eb5ce59249b040416a8050969a6faf
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-18 21:40:04 -0400

    Add logic for sad path if certain attributes are blank on incident creation

--- app/controllers/api/v1/incidents_controller.rb
+++ app/controllers/api/v1/incidents_controller.rb
@@ -7,10 +7,10 @@ class Api::V1::IncidentsController < ApplicationController
 
   def create
     incident = Incident.new(incident_params)
-    if incident.save!
+    if incident.save
       render json: IncidentsSerializer.new(incident), status: :created
     else
-      render json: IncidentCreateErrorSerializer.new
+      validate_inicdent_params
     end
   end
 
@@ -18,5 +18,22 @@ class Api::V1::IncidentsController < ApplicationController
 
   def incident_params
     params.permit(:name, :active, :incident_type, :description, :location, :start_date, :close_date )
+  end
+
+  def validate_inicdent_params
+    if params[:name].nil? || params[:name] == ""
+      error = "Name cannot be blank."
+      render json: ErrorSerializer.new(error)
+    elsif params[:incident_type].nil? || params[:incident_type] == ""
+      error = "Incident type cannot be blank."
+      render json: ErrorSerializer.new(error)
+    elsif params[:location].nil? || params[:location] == ""
+      error = "Location cannot be blank."
+      render json: ErrorSerializer.new(error)
+    elsif params[:start_date].nil? || params[:start_date] == ""
+      error = "Start date cannot be blank."
+      render json: ErrorSerializer.new(error)
+    else
+    end
   end
 end
