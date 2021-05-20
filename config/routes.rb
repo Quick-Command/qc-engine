4727edde56a6535051691e2b0ebcd26f5afa0c6f
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-20 08:28:17 -0400

    Add update to incidents routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :contacts, only: [:show]
-      resources :incidents, only: [:index, :create, :show]
+      resources :incidents, only: [:index, :create, :show, :update]
       # namespace :incidents do
       #   get '/open', to: 'incidents#incidents_by_status'
       # end
