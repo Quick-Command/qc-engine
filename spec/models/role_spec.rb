f6ade21f58e45a3171ee1e3fd25489da75754f0d
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-22 07:35:01 -0400

    Add validation for uniqness of role

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Role, type: :model do
   describe "validations" do
     it { should validate_presence_of :title }
+    it { should validate_uniqueness_of :title }
     it { should have_many(:contact_roles) }
     it { should have_many(:contacts).through(:contact_roles) }
   end
