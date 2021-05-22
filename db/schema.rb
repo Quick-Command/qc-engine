7d7cf84d4124b5a155dbc8faa31b2168a47668da
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-22 05:54:29 -0400

    Add city and state to schema for incidents table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_21_112249) do
+ActiveRecord::Schema.define(version: 2021_05_22_094312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2021_05_21_112249) do
     t.datetime "close_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "city"
+    t.string "state"
   end
 
 end
