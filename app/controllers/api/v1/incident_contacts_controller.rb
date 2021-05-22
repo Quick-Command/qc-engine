d9292f30ecf9b328f983c9d3998561da500c4394
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-22 18:21:18 -0400

    Add edge case logic to assign contact to incident controller

--- app/controllers/api/v1/incident_contacts_controller.rb
+++ app/controllers/api/v1/incident_contacts_controller.rb
@@ -11,8 +11,10 @@ class Api::V1::IncidentContactsController < ApplicationController
       render json: {error: "Contact does not exist"}, status: :not_found
     elsif incident_contact.save
       render json: IncidentContactSerializer.new(contact.first, {params: {distance_miles: distance_miles, distance_minutes: distance_minutes, title: incident_contact_params[:title]}})
-    else
+    elsif incident_contact_params[:title] == nil || incident_contact_params[:title] == ""
       render json: {error: "Title must be assigned"}
+    elsif
+      render json: {error: "Contact is assigned to another incident"}
     end
   end
 
