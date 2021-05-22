aa93883de1c73218ad32ed3bfc72819951f4e31f
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-22 07:31:22 -0400

    Create migration for contact roles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_22_111400) do
+ActiveRecord::Schema.define(version: 2021_05_22_112456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contact_roles", force: :cascade do |t|
+    t.bigint "contact_id"
+    t.bigint "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contact_id"], name: "index_contact_roles_on_contact_id"
+    t.index ["role_id"], name: "index_contact_roles_on_role_id"
+  end
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
@@ -53,6 +62,8 @@ ActiveRecord::Schema.define(version: 2021_05_22_111400) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "contact_roles", "contacts"
+  add_foreign_key "contact_roles", "roles"
   add_foreign_key "incident_contacts", "contacts"
   add_foreign_key "incident_contacts", "incidents"
 end
