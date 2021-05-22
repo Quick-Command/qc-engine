419e142769eeeaf25f8250b831d7bcca32c976b1
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-22 12:32:45 -0400

    Create test file for creating an inicdent-contact

---
+++ spec/requests/api/v1/create_incident_contact_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'Create an Incident Contact Endpoint' do
+  describe 'post request to add a contact to an incident with a specific title/role' do
+    describe 'happy path' do
+      
+    end
+  end
+end
