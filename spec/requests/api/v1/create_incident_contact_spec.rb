eea84c19c94e7aca373e4ccc6ab2bb6ac08a4b57
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-22 17:32:10 -0400

    Finish happy path test for assigning a contact to an incident

--- spec/requests/api/v1/create_incident_contact_spec.rb
+++ spec/requests/api/v1/create_incident_contact_spec.rb
@@ -5,6 +5,34 @@ RSpec.describe 'Create an Incident Contact Endpoint' do
     describe 'happy path' do
       it 'can add a contact to an incident with a specific role' do
         incident = create(:incident, active: true)
+        contact = create(:contact, job_title: "Safety Officer")
+        incident_contact_params = ({ title: "Safety Officer" })
+
+        headers = {"CONTENT_TYPE" => "application/json"}
+        post "/api/v1/incidents/#{incident.id}/contacts/#{contact.id}", headers: headers, params: JSON.generate(incident_contact_params)
+
+        result = JSON.parse(response.body, symbolize_names: true)
+        incident_contact_1 = IncidentContact.last
+
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(result).to be_a(Hash)
+        expect(result[:data]).to be_a(Hash)
+        expect(result[:data].count).to eq(3)
+        expect(result[:data]).to have_key(:id)
+        expect(result[:data][:id]).to be_a(String)
+        expect(result[:data][:id]).to eq(contact.id.to_s)
+        expect(result[:data]).to have_key(:type)
+        expect(result[:data][:type]).to eq("incident_contact")
+        expect(result[:data]).to have_key(:attributes)
+        expect(result[:data][:attributes]).to be_a(Hash)
+        expect(result[:data][:attributes].count).to eq(3)
+        expect(result[:data][:attributes]).to have_key(:title)
+        expect(result[:data][:attributes][:title]).to be_a(String)
+        expect(result[:data][:attributes]).to have_key(:distance)
+        expect(result[:data][:attributes][:distance]).to be_a(String)
+        expect(result[:data][:attributes]).to have_key(:time_to_incident)
+        expect(result[:data][:attributes][:time_to_incident]).to be_a(String)
       end
     end
   end
