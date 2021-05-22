eaf19403aac679321ecefcceacc671b36c1416ff
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-22 13:09:11 -0400

    Update test

--- spec/requests/api/v1/create_incident_contact_spec.rb
+++ spec/requests/api/v1/create_incident_contact_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'Create an Incident Contact Endpoint' do
   describe 'post request to add a contact to an incident with a specific title/role' do
     describe 'happy path' do
-      
+      it 'can add a contact to an incident with a specific role' do
+        incident = create(:incident, active: true)
+      end
     end
   end
 end
