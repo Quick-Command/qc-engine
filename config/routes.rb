e35e492b8c39e2bfdab8c286f0afeedaa91a84d8
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-23 09:16:23 -0400

    Handroll route to SearchController

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/api/v1/incidents/:incident_id/contacts/:contact_id', to: 'api/v1/incident_contacts#assign_contact_to_incident'
   get '/api/v1/incidents/:incident_id/contacts/:contact_id', to: 'api/v1/incident_contacts#show'
+  get '/api/v1/contacts/contact_search', to: 'api/v1/search#contact_role_search'
+
+
   namespace :api do
     namespace :v1 do
       resources :contacts, only: [:show, :create]
