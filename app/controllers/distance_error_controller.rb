a79069501ddc691eee9a50f2532214e7d02a98cc
Jeremiah Michlitsch <jeremiahmichlitsch@gmail.com>
2021-05-24 17:02:36 -0600

    Finish sad path testing for distance requests
    Co-authored-by: Katy La Tour <23460878+klatour324@users.noreply.github.com>

--- app/controllers/distance_error_controller.rb
+++ app/controllers/distance_error_controller.rb
@@ -1,6 +1,4 @@
 class DistanceErrorController < ApplicationController
-
-  rescue_from JSON::ParserError, with: :render_unavailable
   rescue_from TypeError, with: :type_error
 
   def error(message = "Invalid request error. Please check the request.")
@@ -8,10 +6,7 @@ class DistanceErrorController < ApplicationController
   end
 
   def type_error(exception)
-    render json: { error: 'Please enter a valid location with city and state.' }, status: :bad_request
+    render json: { error: 'Locations does not exist. Please enter valid locations.' }, status: :bad_request
   end
 
-  def render_unavailable(exception)
-    render json: { error: 'Location does not exist. Please enter valid location.' }, status: :bad_request
-  end
 end
--- app/facades/distance_facade.rb
+++ app/facades/distance_facade.rb
@@ -3,4 +3,18 @@ class DistanceFacade
     distance = WeatherService.fetch_distance(origin, destination)
     Distance.new(distance)
   end
+
+  def self.invalid_origin?(params)
+    return true if params[:origin].nil?
+
+    city, state = params[:origin].split(',')
+    params[:origin] == '' || city.blank? || state.blank?
+  end
+
+  def self.invalid_destination?(params)
+    return true if params[:destination].nil?
+
+    city, state = params[:destination].split(',')
+    params[:destination] == '' || city.blank? || state.blank?
+  end
 end
