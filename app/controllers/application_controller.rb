c7eee4bcb4eac315892bb060c86d7dc734457b15
Katy La Tour <klatour324@gmail.com>
2021-05-24 15:36:21 -0500

    Add rendered error messages to application controller
    Co-authored-by: Jeremiah Michlitsch <61896482+jmichlitsch@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  rescue_from JSON::ParserError, with: :render_unavailable
+
+  def error(message = "Invalid request error. Please check the r")
+    render json: { error: message }, status: :bad_request
+  end
+
+  def render_unavailable(exception)
+    render json: { error: 'Location does not exist. Please enter valid location.' }, status: :bad_request
+  end
 end
