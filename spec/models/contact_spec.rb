7c558cafd4244a620b942d0d365275433acee43f
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-05-25 18:20:44 -0400

    Add test to confirm assigned_to_active_incidents works correctly

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -15,4 +15,34 @@ RSpec.describe Contact, type: :model do
     it { should have_many(:contact_roles) }
     it { should have_many(:roles).through(:contact_roles) }
   end
+
+  describe "instance variables" do
+    describe "#assigned_to_active_incident?" do
+      it "returns true if a contact is assigned to an active incident" do
+        role_1 = create(:role, title: "Incident Commander")
+        role_2 = create(:role, title: "Safety Officer")
+        incident = create(:incident, active: true)
+        incident_2 = create(:incident, active: false)
+        contact_1 = create(:contact, job_title: "Incident Commander")
+        contact_2 = create(:contact, job_title: "Incident Commander")
+        contact_3 = create(:contact, job_title: "Incident Commander")
+        ic = IncidentContact.create({incident_id: incident.id, contact_id: contact_3.id, title: "Incident Commander"})
+        ic2 = IncidentContact.create({incident_id: incident_2.id, contact_id: contact_2.id, title: "Incident Commander"})
+        ic3 = IncidentContact.create({incident_id: incident_2.id, contact_id: contact_3.id, title: "Incident Commander"})
+        contact_4 = create(:contact, job_title: "Safety Officer")
+        contact_5 = create(:contact, job_title: "Safety Officer")
+        contact_1.roles << role_1
+        contact_2.roles << role_1
+        contact_3.roles << role_1
+        contact_4.roles << role_2
+        contact_5.roles << role_2
+
+        expect(contact_3.assigned_to_active_incident?).to eq(true)
+        expect(contact_1.assigned_to_active_incident?).to eq(false)
+        expect(contact_2.assigned_to_active_incident?).to eq(false)
+        expect(contact_4.assigned_to_active_incident?).to eq(false)
+        expect(contact_5.assigned_to_active_incident?).to eq(false)
+      end
+    end
+  end
 end
